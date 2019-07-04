d48a8d5def8479a3bcd28e9857f7fa6cef9b5492
liwenliang <792831375@qq.com>
2019-07-04 20:00:20 +0800

    h265 的 demo

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,9 +1,23 @@
-const webpack = require('webpack')
-
 module.exports = {
   entry: './src/main.js',
   output: {
     filename: './dist/bundle.js'
+  },
+  devServer: {
+    proxy: {
+      '/h265': {
+        target: 'http://qiniu-store.houjiyi.com/', // 目标接口域名
+        changeOrigin: true, // 是否跨域
+        pathRewrite: {
+          '^/h265': '/h265' // 重写接口
+        }
+      }
+    },
+
+    // Various Dev Server settings
+    host: '10.30.0.228', // can be overwritten by process.env.HOST
+    port: 9802,
+    open: true
   },
   module: {
     rules: [
